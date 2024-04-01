a2a91914ac09a7010c03d2c02c514e62e4429e51
ahdma@mail.ru <ahdma@mail.ru>
2024-04-01 14:52:43 +0300

    feath: save logs to files

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,4 +1,7 @@
 import { Injectable, Logger, LoggerService } from '@nestjs/common';
+import * as fs from 'fs/promises';
+import * as path from 'path';
+
 enum LoggerLevel {
   'fatal' = 0,
   'error',
@@ -27,33 +30,71 @@ export class CustomLogger implements LoggerService {
       this.logger.warn(message);
     }
   }
-  fatal?(type, error) {
+  async fatal?(type, error) {
     if (this.logLevels >= 0) {
-      this.logger.fatal(`\n${type} \n${error}`);
+      const message = `\n${type} \n${error}`;
+      this.logger.fatal(message);
+      await this.writeLogsToFile(message, 'logs');
+      await this.writeLogsToFile(message, 'fatales');
     }
   }
 
-  logRequest(request: any): void {
-    this.log(
-      `\n[REQUEST]:\nMethod: ${request.method}\nUrl:${request.url}\nQuery: ${JSON.stringify(request.query)}\nBody: ${JSON.stringify(request.body)}\n`,
-    );
+  async logRequest(request: any) {
+    const message = `\n[REQUEST]:\nMethod: ${request.method}\nUrl:${request.url}\nQuery: ${JSON.stringify(request.query)}\nBody: ${JSON.stringify(request.body)}\n`;
+    this.log(message);
+    await this.writeLogsToFile(message, 'logs');
   }
 
-  logResponse(body, code): void {
-    this.log(
-      `\n[RESPONSE]:\nStatus code: ${code}\nBody: ${body ? JSON.stringify(body) : null}\n`,
-    );
+  async logResponse(body, code) {
+    const message = `\n[RESPONSE]:\nStatus code: ${code}\nBody: ${body ? JSON.stringify(body) : null}\n`;
+    this.log(message);
+    await this.writeLogsToFile(message, 'logs');
   }
 
-  logError(res, body): void {
-    this.error(`\n[ERROR]:\nstatusCode: ${res.statusCode}\nBody: ${body}\n`);
+  async logError(res, body) {
+    const message = `\n[ERROR]:\nstatusCode: ${res.statusCode}\nBody: ${body}\n`;
+    this.error(message);
+    await this.writeLogsToFile(message, 'errors');
+    await this.writeLogsToFile(message, 'logs');
   }
 
-  logWarn(res, body): void {
-    this.warn(`\n[WARN]:\nstatusCode: ${res.statusCode}\nBody: ${body}\n`);
+  async logWarn(res, body) {
+    const message = `\n[WARN]:\nstatusCode: ${res.statusCode}\nBody: ${body}\n`;
+    this.warn(message);
+    await this.writeLogsToFile(message, 'warns');
+    await this.writeLogsToFile(message, 'logs');
   }
 
-  //saveLog(message: string) {
+  async writeLogsToFile(logMessage: string, logFile: string) {
+    const logFilePath = path.join('./logs/', logFile + '.log');
+    try {
+      let fileExists = true;
+      try {
+        await fs.access(logFilePath);
+      } catch (error) {
+        fileExists = false;
+      }
 
-  //}
+      if (!fileExists) {
+        await fs.writeFile(logFilePath, '');
+      }
+
+      const stats = await fs.stat(logFilePath);
+      const fileSizeInBytes = stats.size;
+      if (fileSizeInBytes > +process.env.MAX_FILE_SIZE_MB * 1024) {
+        const timestamp = new Date().toISOString().replace(/:/g, '-');
+        const newFilePath = path.join('./logs/', `${timestamp}_${logFile}.log`);
+
+        await fs.rename(logFilePath, newFilePath);
+        await fs.writeFile(logFilePath, '');
+      }
+
+      await fs.appendFile(
+        logFilePath,
+        `[${new Date().toISOString()}]` + logMessage + '\n',
+      );
+    } catch (error) {
+      this.error('Error while writing logs to file:\n' + JSON.stringify(error));
+    }
+  }
 }
